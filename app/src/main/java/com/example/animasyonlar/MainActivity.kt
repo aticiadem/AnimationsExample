fd3be0d23ea4e8b80ee8f403fdb909e22b3e3652
adematici <adematicis41@gmail.com>
2021-07-24 22:14:53 +0300

    MotionLayout added

--- app/src/main/java/com/example/animasyonlar/MainActivity.kt
+++ app/src/main/java/com/example/animasyonlar/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.animasyonlar
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.BounceInterpolator
@@ -11,6 +12,7 @@ import com.example.animasyonlar.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var fabState = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +32,9 @@ class MainActivity : AppCompatActivity() {
             // accelerateAnimation()
             // repeatAnimation()
             // multipleTogetherAnimation()
-            multipleSequentiallyAnimation()
+            // multipleSequentiallyAnimation()
+            val intent = Intent(this,MotionActivity::class.java)
+            startActivity(intent)
         }
 
     }
